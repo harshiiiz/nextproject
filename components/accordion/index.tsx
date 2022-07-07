bfef8836b3ec21211d4adf84283d0886eef5a042
“[harshita <harshitasahu3110@gmail.com>
2022-07-08 04:52:39 +0530

    ui

--- components/accordion/index.tsx
+++ components/accordion/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import styles from '../accordion/Accordion.module.scss'
+import styles from '../accordion/Accordion.module.scss';
+import Image from 'next/image';
+
 
 const Accordion = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
@@ -9,7 +11,7 @@ const Accordion = ({ title, content }) => {
         
       <div className={styles.acctitle} onClick={() => setIsActive(!isActive)}>
         <p className={isActive ? 'blue' : 'black'}>{title}</p>
-        <div className={isActive ? 'minus' : 'plus'}></div>
+        <Image src={isActive ? '/minus.svg' : '/plusbig.svg'} width={18} height={16}></Image>
         </div>
       
       {isActive && <div className={styles.acccontent}>{content}</div>}
